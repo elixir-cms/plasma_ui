a2e755264efdeb2eb951739ab467013141f1f901
Jason <jason@fullsteamlabs.com>
2021-05-31 15:10:26 -0400

    Adds accordion component

---
+++ lib/plasma_ui_web/components/accordion.ex
@@ -0,0 +1,25 @@
+defmodule PlasmaUiWeb.Components.Accordion do
+  @moduledoc """
+  TODO
+  """
+
+  use Surface.Component
+
+  slot default, required: true
+
+  prop title, :string, required: true
+
+  def(render(assigns)) do
+    ~H"""
+    <div x-data="{ open: 0 }">
+      <div @click="open = !open" class="accordion">
+        <p>{{ @title }}</p>
+        <span :class="open == 1 ? 'fa-chevron-down' : 'fa-chevron-up'" class="fas" />
+      </div>
+      <div x-show="open == 1" class="border p-4">
+        <slot />
+      </div>
+    </div>
+    """
+  end
+end
