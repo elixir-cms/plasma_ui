26d95ed89b1917c897643a830e3966c0d37cb876
Jason <jason@fullsteamlabs.com>
2021-10-15 18:03:01 -0400

    Adds moduledoc and style prop to accordion

--- lib/plasma_ui_web/components/accordion.ex
+++ lib/plasma_ui_web/components/accordion.ex
@@ -1,17 +1,21 @@
 defmodule PlasmaUiWeb.Components.Accordion do
   @moduledoc """
-  TODO
+  A simple accordion. Props include title and style.
+  Title is shown as the toggle text and style used as an attribute on the parent element.
+  There is one slot, the default slot, which is used as accordion content.
   """
 
   use Surface.Component
 
-  slot default, required: true
+  slot(default, required: true)
 
-  prop title, :string, required: true
+  prop(style, :string, default: "")
+
+  prop(title, :string, required: true)
 
   def(render(assigns)) do
     ~F"""
-    <div x-data="{ open: 0 }">
+    <div x-data="{ open: 0 }" style={@style}>
       <div @click="open = !open" class="accordion">
         <p>{@title}</p>
         <span :class="open == 1 ? 'fa-chevron-down' : 'fa-chevron-up'" class="fas" />
