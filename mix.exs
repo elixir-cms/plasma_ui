4d1b65a5b2b62f27ba997f1881f329e630b174d3
Jason <jason@fullsteamlabs.com>
2021-10-15 18:00:44 -0400

    Updates deps

--- mix.exs
+++ mix.exs
@@ -44,26 +44,26 @@ defmodule PlasmaUi.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.8"},
-      {:phoenix_ecto, "~> 4.1"},
-      {:ecto_sql, "~> 3.4"},
-      {:postgrex, ">= 0.0.0"},
-      {:phoenix_live_view, "~> 0.15.1"},
-      {:floki, ">= 0.27.0", only: :test},
-      {:phoenix_html, "~> 2.11"},
-      {:phoenix_live_reload, "~> 1.2", only: [:dev, :test]},
-      {:phoenix_live_dashboard, "~> 0.4"},
-      {:telemetry_metrics, "~> 0.4"},
-      {:telemetry_poller, "~> 0.4"},
-      {:gettext, "~> 0.11"},
-      {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
+      {:ecto_entity, path: "../ecto_entity"},
+      {:ecto_sql, "~> 3.4"},
+      {:floki, ">= 0.27.0", only: :test},
+      {:gettext, "~> 0.11"},
+      {:jason, "~> 1.0"},
+      {:phoenix, "~> 1.5.8"},
+      {:phoenix_ecto, "~> 4.1"},
+      {:phoenix_html, "~> 2.11"},
+      {:phoenix_live_dashboard, "~> 0.4"},
+      {:phoenix_live_reload, "~> 1.2", only: [:dev, :test]},
+      {:phoenix_live_view, "~> 0.15.1"},
+      {:plug_cowboy, "~> 2.0"},
+      {:postgrex, ">= 0.0.0"},
       {:surface, "~> 0.5.1"},
       {:surface_catalogue, "~> 0.1.0", only: [:dev, :test]},
       {:surface_formatter, "~> 0.5.1"},
-      {:ecto_entity, path: "../ecto_entity"}
+      {:telemetry_metrics, "~> 0.4"},
+      {:telemetry_poller, "~> 0.4"}
     ]
   end
 
