9c00077d8887a1df09816bb41cebebacfd8fcea7
Jason <jason@fullsteamlabs.com>
2021-11-09 15:17:50 -0500

    Adds moduledoc to accordion component example

--- lib/plasma_ui_web/catalogue/plasma_ui/components/accordion/example01.ex
+++ lib/plasma_ui_web/catalogue/plasma_ui/components/accordion/example01.ex
@@ -1,4 +1,8 @@
 defmodule PlasmaUiWeb.Components.Accordion.Example01 do
+  @moduledoc """
+  A surface catalogue example component for PlasmaUiWeb.Components.Accordion.
+  """
+
   use Surface.Catalogue.Example,
     catalogue: PlasmaUiWeb.Components.Catalogue,
     subject: PlasmaUiWeb.Components.Accordion,
